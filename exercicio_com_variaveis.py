de8ba59a4b9d42b8fd70881df67dbb1e159f5d82
JGiotech <giovannabarbosah38@gmail.com>
2026-02-24 18:56:17 -0300

    Revisando aulas

---
+++ exercicio_com_variaveis.py
@@ -0,0 +1,13 @@
+nome = 'Gio'
+sobrenome = 'Barbosa'
+idade = 21
+ano_nascimento = 2025 - idade
+maior_de_idade = idade >= 21
+altura_metros = 1.56
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
