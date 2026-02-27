cc045fe93edec7bc513a403bf55b7024cfd5675d
JGiotech <giovannabarbosah38@gmail.com>
2026-02-27 18:30:43 -0300

    Revisando condicionais if, elif e else do python

---
+++ condicionais.py
@@ -0,0 +1,32 @@
+# entrada= input('Voce quer entrar ou sair? ')
+
+# if entrada == 'entrar':
+#     print('voce entrou no sistema.')
+
+# elif entrada ==  'sair':
+#     print('voce saiu do sistema.')
+
+# else:
+#     print('voce nao digitou nem entrar e nem sair.')
+
+    
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('codigo para condicao 1')
+elif condicao2:
+    print('codigo para condicao 2')
+elif condicao3:
+    print('codigo para condicao 3')
+elif condicao4:
+    print('codigo para condicao 4')
+else:
+    print('nehuma condicao foi satisfeita')
+
+if 10 == 10:
+    print('outro if')
+
+print('Fora do if')
